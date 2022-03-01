52f661bafdb8eb6705bfc1ed9ec1830684298210
Carroth23 <wlsrb230@gmail.com>
2022-03-01 17:54:04 +0900

    함수

---
+++ Proc/src/boj/N15596.java
@@ -0,0 +1,12 @@
+package boj;
+
+public class N15596 {
+	
+	public long sum(int[] a) {
+		long sum = 0;
+		for(long i : a) {
+			sum += i;
+		}
+		return sum;
+	}
+}
--- Proc/src/boj/N2562최댓값.java
+++ Proc/src/boj/N2562최댓값.java
@@ -6,7 +6,22 @@ public class N2562최댓값 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+		int[] num = new int[9];
+		for(int i = 0; i < num.length; i++) {
+			num[i] = sc.nextInt();
+		}
+		sc.close();
+		int count = 0;
+		int max = 0;
+		int number = 0;
+		for(int i : num) {
+			count++;
+			if(i > max) {
+				number = count;
+				max = i;
+			}
+		}
+		System.out.println(max + " " + number);
 	}
 
 }
