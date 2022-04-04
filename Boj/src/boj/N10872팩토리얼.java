5312bfec00bfadf2f05e31f90b3df875c45f0465
Carroth23 <wlsrb230@gmail.com>
2022-04-05 00:03:44 +0900

    N10872팩토리얼

---
+++ Boj/src/boj/N10872팩토리얼.java
@@ -0,0 +1,18 @@
+package boj;
+
+import java.util.Scanner;
+
+public class N10872팩토리얼 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int sum = 1;
+		for(int i = 1; i <= n; i++) {
+			sum *= i;
+		}
+		System.out.println(sum);
+	}
+
+}
