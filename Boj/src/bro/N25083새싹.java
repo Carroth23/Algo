899bc740eff396fc0a058f3e8b78e81d78724059
Carroth23 <wlsrb230@gmail.com>
2022-05-01 00:28:59 +0900

    새싹

---
+++ Boj/src/bro/N25083새싹.java
@@ -0,0 +1,14 @@
+package bro;
+
+public class N25083새싹 {
+
+	public static void main(String[] args) {
+		System.out.println("         ,r\'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+
+}
